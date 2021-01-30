4543cfb0eeb3cfe0ce5044b89e06997bcf6260f7
unknown <you@example.com>
2021-01-30 23:38:03 +0100

    malek

---
+++ Projet_MounaMalek/src/app/formateurs/formateurs.ts
@@ -0,0 +1,20 @@
+import { Formateur } from "./formateur";
+
+export const FORMATEURSLIST: Formateur[] = [
+    {
+        id: 1,
+        name: 'Malek',
+        firstname: 'Ferchichi',
+        adress: 'Bizerte',
+        email: 'ferchichimalek@gmail.com',
+        expertise: 'Angular'
+    },
+    {
+        id: 2,
+        name: 'Mouna',
+        firstname: 'Baccouch',
+        adress: 'Sfax',
+        email: 'MounaBaccouch@gmail.com',
+        expertise: 'Mobile'
+    }
+]
---
+++ Projet_MounaMalek/src/app/participants/participants.ts
@@ -0,0 +1,24 @@
+import { Participant } from "./participant";
+
+export const PARTICIPANTSLIST: Participant[] = [
+    {
+        id: 1,
+        cin : '11225544',
+        name: 'Malek',
+        firstname: 'Ferchichi',
+        adress: 'Bizerte',
+        email: 'malekferchichi57@gmail.com',
+        societe: 'ENICarthage',
+        poste: "developer"
+    },
+    {
+        id: 2,
+        cin : '15894723',
+        name: 'Mouna',
+        firstname: 'Baccouch',
+        adress: 'Sfax',
+        email: 'MounaBaccouch@gmail.com',
+        societe: 'ENICarthage',
+        poste: "developer"
+    }
+]
